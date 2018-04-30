4220f07da5014ba74f40c3616f44cd1f591d74d2
Gonzo <edwin.gonzales.n@gmail.com>
2018-04-30 17:00:55 -0500

    deleted some unnecessary files.  Good to go.

--- src/main/java/com/tgj/eventaid/Repositories/UserRepository.java
+++ src/main/java/com/tgj/eventaid/Repositories/UserRepository.java
@@ -1,11 +1,11 @@
 package com.tgj.eventaid.Repositories;
 
-import com.tgj.eventaid.models.User;
+import com.tgj.eventaid.models.Users;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long>{
-    User findById(Long id);
-    User findByEmail(String email);
+public interface UserRepository extends CrudRepository<Users, Long>{
+    Users findById(Long id);
+    Users findByEmail(String email);
 }
