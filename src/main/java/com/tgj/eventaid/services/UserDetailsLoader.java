0768c7fc9df075e1870fe70404efb2566dcc66c3
Tristan Crawford <tristanthtcrawford@gmail.com>
2018-05-01 11:48:20 -0500

    Changed Plurality, Fixed Templates, Added Security

---
+++ src/main/java/com/tgj/eventaid/services/UserDetailsLoader.java
@@ -0,0 +1,27 @@
+package com.tgj.eventaid.services;
+import com.tgj.eventaid.models.User;
+import com.tgj.eventaid.models.UserWithRoles;
+import com.tgj.eventaid.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsLoader implements UserDetailsService {
+
+    private final UserRepository users;
+
+    public UserDetailsLoader(UserRepository users) {
+        this.users = users;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = users.findByEmail(email);
+        if(user == null) {
+            throw new UsernameNotFoundException(String.format("User '%s' not found!", email));
+        }
+        return new UserWithRoles(user);
+    }
+}
