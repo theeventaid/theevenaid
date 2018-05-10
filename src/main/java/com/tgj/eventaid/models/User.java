d57c7fe14d92f5ffa688363f50b5e6750252ee18
Gonzo <edwin.gonzales.n@gmail.com>
2018-05-10 13:43:32 -0500

    almost done with stripe

--- src/main/java/com/tgj/eventaid/models/User.java
+++ src/main/java/com/tgj/eventaid/models/User.java
@@ -48,7 +48,7 @@ public class User {
     @Column
     private LocalDateTime created_on;
 
-    @Column(nullable = false)
+    @Column() // switched this to nullable
     private boolean owner;
 
     @OneToMany(mappedBy = "user_id")
